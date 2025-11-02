ac87aed4194a4a375f8417f3a714292b69e97d94
minco <mail@drchi.co.kr>
2025-11-03 00:18:28 +0900

    fix: change color

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { VolumeControl } from './components/volume-control.tsx'
 function App() {
     return (
         <div className="w-full h-full flex flex-col justify-center items-center gap-5">
-            <h1 className="text-4xl">Remodian</h1>
+            <h1 className="text-4xl title">Remodian</h1>
             <PowerControl />
             <VolumeControl />
         </div>
